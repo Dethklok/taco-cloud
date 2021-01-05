c64ec46354e1bd20ab60ec4696c0c18855b5e8e5
Igor Spaschanskiy <x.firedragon@gmail.com>
2021-01-05 18:44:23 +0500

    init application with simple controllers, views, models

---
+++ src/main/java/sin/taco/TacoApplication.java
@@ -0,0 +1,13 @@
+package sin.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoApplication.class, args);
+	}
+
+}
