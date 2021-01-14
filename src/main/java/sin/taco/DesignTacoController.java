11f62e85955505282950ccb0b3e5b8545e3225b7
Dethklok <x.firedragon@gmail.com>
2021-01-14 10:09:37 +0500

    Card number validation and web config

--- src/main/java/sin/taco/DesignTacoController.java
+++ src/main/java/sin/taco/DesignTacoController.java
@@ -3,6 +3,7 @@ package sin.taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,8 @@ import java.util.stream.Collectors;
 
 import sin.taco.Ingredient;
 import sin.taco.Ingredient.Type;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -47,7 +50,11 @@ public class DesignTacoController {
   }
 
   @PostMapping
-  public String processDesign(Taco taco) {
+  public String processDesign(@Valid Taco taco, Errors errors) {
+    if (errors.hasErrors()) {
+      return "design";
+    }
+
     log.info("Processing design: " + taco);
     return "redirect:/orders/current";
   }
