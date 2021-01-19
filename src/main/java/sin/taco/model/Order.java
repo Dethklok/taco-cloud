4477c039abfe528f3f1d49381c45d774b1273b21
miracle <x.firedragon@gmail.com>
2021-01-19 22:12:02 +0500

    Add user info into Order

--- src/main/java/sin/taco/model/Order.java
+++ src/main/java/sin/taco/model/Order.java
@@ -55,6 +55,9 @@ public class Order implements Serializable {
   @ManyToMany(targetEntity = Taco.class)
   private List<Taco> tacos = new ArrayList<>();
 
+  @ManyToOne
+  private User user;
+
   public void addTaco(Taco saved) {
     tacos.add(saved);
   }
