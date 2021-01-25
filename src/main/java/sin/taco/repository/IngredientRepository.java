03faea4d6040d2988ee40b2fc6edb4e60fa46ddc
miracle <x.firedragon@gmail.com>
2021-01-25 16:58:41 +0500

    clean project

--- src/main/java/sin/taco/repository/IngredientRepository.java
+++
@@ -1,8 +0,0 @@
-package sin.taco.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import sin.taco.model.Ingredient;
-
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-
-}
--- src/main/java/sin/taco/repository/TacoRepository.java
+++
@@ -1,8 +0,0 @@
-package sin.taco.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import sin.taco.model.Taco;
-
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-
-}
