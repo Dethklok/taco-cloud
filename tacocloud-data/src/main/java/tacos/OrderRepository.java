074b9cc012277cb6cbb90a48413c809c8a6db7de
miracle <x.firedragon@gmail.com>
2021-02-01 20:15:57 +0500

    Added Repositories

---
+++ tacocloud-data/src/main/java/tacos/OrderRepository.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+
+  List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
+
+}
